c8d8875dcc06af333ccf39e61b859786eefdc9dc
Kirill Bobykin <qelphybox@gmail.com>
2025-12-15 17:06:47 +0400

    update models

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,2 +1,6 @@
 class Hobby < ApplicationRecord
+  has_many :person_hobbies, dependent: :destroy
+  has_many :people, through: :person_hobbies
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ApplicationRecord
+  has_many :contacts, dependent: :destroy
+  has_many :person_hobbies, dependent: :destroy
+  has_many :hobbies, through: :person_hobbies
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birth_date, presence: true
 end
