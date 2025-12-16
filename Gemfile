ad93521eeae8f89658a6334f8b61af26b378460f
Kirill Bobykin <qelphybox@gmail.com>
2025-12-16 15:08:31 +0400

    add seeds

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ group :test do
 end
 
 gem "cssbundling-rails", "~> 1.4"
+
+gem "faker", "~> 3.5"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,51 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Person.destroy_all
+Hobby.destroy_all
+Contact.destroy_all
+PersonHobby.destroy_all
+
+puts "Creating hobbies..."
+hobbies = []
+150.times do |i|
+  hobbies << Hobby.create!(name: Faker::Hobby.unique.activity)
+end
+puts "Created #{hobbies.count} hobbies"
+
+puts "Creating people..."
+people = []
+1000.times do |i|
+  people << Person.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    birth_date: Faker::Date.birthday(min_age: 18, max_age: 80)
+  )
+end
+puts "Created #{people.count} people"
+
+puts "Connecting people with hobbies..."
+# Каждый человек владеет 10 хобби
+people.each do |person|
+  start_index = person.id % 150
+  10.times do |offset|
+    hobby_index = (start_index + offset) % 150
+    PersonHobby.create!(
+      person: person,
+      hobby: hobbies[hobby_index]
+    )
+  end
+end
+puts "Connected people with hobbies"
+
+puts "Creating contacts..."
+# У каждого человека по 2 контакта
+people.each do |person|
+  2.times do
+    contact_type = [ "email", "phone" ].sample
+    contact_value = contact_type == "email" ? Faker::Internet.email : Faker::PhoneNumber.phone_number
+    Contact.create!(
+      person: person,
+      contact_type: contact_type,
+      contact_value: contact_value
+    )
+  end
+end
+puts "Created #{Contact.count} contacts"
